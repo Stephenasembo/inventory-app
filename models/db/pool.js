eeae54a6a9045246e1d2f801984b0bcfea4ffccb
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-06-24 20:29:23 +0300

    Feat: Add Configuration For Different Environments
    It is now easier to run the app in either development or production
    environment

--- models/db/pool.js
+++ models/db/pool.js
@@ -3,13 +3,16 @@ require('dotenv').config()
 const fs = require('fs');
 const { Pool } = require('pg');
 
-const localDb = process.env.LOCAL_DB;
-const remoteDb = process.env.REMOTE_DB;
-
-module.exports = new Pool ({
-  connectionString: remoteDb,
+const prodConfig = {
+  connectionString: process.env.REMOTE_DB,
   ssl: {
     rejectUnauthorized: true,
     ca: fs.readFileSync("./ca.pem").toString(),
   },
-});
+}
+
+const devConfig = {
+  connectionString: process.env.LOCAL_DB,
+}
+
+module.exports = new Pool (process.env.NODE_ENV === 'prod' ? prodConfig : devConfig);
