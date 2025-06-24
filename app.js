79d44caef69f41503a4b1929f367d7063c41ed6f
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-06-24 21:12:09 +0300

    FeaT: Add Styling To HTML Pages

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ const PORT = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+const assetsPath = path.join(__dirname, 'public')
+
 app.use((express.urlencoded({extended: true})));
+app.use(express.static(assetsPath));
 app.use('/', indexRouter);
 app.use('/categories', categoriesRouter);
 app.use('/items', itemsRouter);
